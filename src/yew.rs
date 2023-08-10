b2c34e96db89aed20f17af0f76117e3823d07c45
Patrick Elsen <pelsen@xfbs.net>
2023-08-10 20:06:46 +0200

    Updates

--- src/yew.rs
+++ src/yew.rs
@@ -1,7 +1,9 @@
 use crate::{CacheKey, RcValue};
 use async_trait::async_trait;
 use prokio::time::sleep;
-use std::{any::Any, collections::BTreeMap, fmt::Debug, rc::Rc, sync::Mutex, time::Duration};
+use std::{
+    any::Any, collections::BTreeMap, error::Error, fmt::Debug, rc::Rc, sync::Mutex, time::Duration,
+};
 use yew::{
     functional::{UseStateHandle, UseStateSetter},
     prelude::*,
@@ -61,11 +63,24 @@ impl Entry {
     }
 }
 
+/// Represents some action that can be cached.
+///
+/// The action has one associated type called [`Value`]. This is the value of data that this action
+/// returns. That data is cached, so that future invocations of this item can reuse the previously
+/// computed data.
+///
+/// The cached data here can be anything, but typically is a network request. The resulting value
+/// is typically the response type of the network request.
 #[async_trait(?Send)]
 pub trait CacheItem: CacheKey + Clone + Ord {
     type Value: Clone + Debug + PartialEq + 'static;
+    type Error: Debug + Error + 'static;
 
     async fn send(&self) -> Result<Self::Value, ()>;
+
+    fn superset(&self) -> Vec<Self> {
+        vec![]
+    }
 }
 
 #[derive(Clone, Default)]
